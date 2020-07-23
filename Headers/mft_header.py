ed982fe50f2531f0594e8c890c92894944fb47ee
Dan Arad <dan1994@gmail.com>
2020-07-23 17:11:55 +0300

    CR comments

--- Headers/mft_header.py
+++ Headers/mft_header.py
@@ -1,7 +1,12 @@
 import struct
 from fix_up_array import FixUpArray
 
+# CR: [design] Putting all headers in a single place is not logically correct.
+# It makes more sense to put each header near its encapsulating object.
+# CR: [conventions] Packages should be lower snake case
 
+# CR: [finish] Is this a generic MFTEntry class? The name MftHeader makes me
+# think it's related specifically to the MFT file
 class MftHeader:
 
     FORMAT = r'4xHH12xHH'
@@ -24,8 +29,14 @@ class MftHeader:
 
     @property
     def is_allocated(self):
+        # CR: [implementation] It would be better to do
+        # return self.allocated_flag & 0x1
         return self.allocated_flag % 2 == 1
 
+    # CR: [design] Is this the correct place to handle fixup arrays? Is it
+    # unique to the MFT or is it a per entry thing?
+    # CR: [finish] The name is a bit cumbersome
+    # CR: [design] Should this be called from __init__?
     def get_origin_mft_entry_bytes(self, mft_entry_bytes: bytes) -> bytes:
         """
         Replace the fixup value in the entry with the original value
